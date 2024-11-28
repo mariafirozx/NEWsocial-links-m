dd3add20e76bc450ec8c2ae399821791acc03d0a
Maria <mariafiroz9@gmail.com>
2024-11-28 18:44:24 +0800

    changes

--- factralTree.js
+++ factralTree.js
@@ -1,30 +1,87 @@
 let angle;
 
-function setup() {
-  const canvas = createCanvas(400, 400);
-  canvas.parent('sketch-container');
-  stroke(255);
+const tree1 = function(sketch){
 
+     sketch.setup = function() {
+        const canvas = sketch.createCanvas(400, 400);
+        canvas.parent('sketch-container');
+        sketch.stroke(255);
+      
+    }
+       sketch.draw = function(){
+        sketch.clear();
+        angle = sketch.sin(sketch.frameCount * 0.01) * sketch.QUARTER_PI; // Smooth oscillation -> MAKES IT MOVE DYNAMICALLY 
+        sketch.translate(sketch.width / 2, sketch.height);
+        sketch.branch(100);
+      }
+
+    sketch.branch = function(len) {
+        sketch.line(0, 0, 0, -len);
+        sketch.translate(0, -len);
+        if (len > 4) {
+            sketch.push();
+            sketch.rotate(angle); // Rotate right
+            sketch.branch(len * 0.67);
+            sketch.pop();
+            sketch.push();
+            sketch.rotate(-angle); // Rotate left
+            sketch.branch(len * 0.67);
+            sketch.pop();
+        }
+    }
 }
 
-function draw() {
-  clear();
-  angle = sin(frameCount * 0.01) * QUARTER_PI; // Smooth oscillation
-  translate(width / 2, height);
-  branch(100);
+new p5(tree1);
+// function setup() {
+//   const canvas = createCanvas(400, 400);
+//   canvas.parent('sketch-container');
+//   stroke(255);
+
+// }
+
+// function draw() {
+//   clear();
+//   angle = sin(frameCount * 0.01) * QUARTER_PI; // Smooth oscillation -> MAKES IT MOVE DYNAMICALLY 
+//   translate(width / 2, height);
+//   branch(100);
+//   branch2(100);
+// }
+
+
+const tree2 = function(sketch){
+    let reverse = false;
+
+    sketch.setup = function() {
+       const canvas = sketch.createCanvas(400, 400);
+       canvas.parent('sketch-container2');
+       sketch.stroke(255);
+     
+   }
+      sketch.draw = function(){
+       sketch.clear();
+       angle = sketch.sin(sketch.frameCount * 0.01) * sketch.QUARTER_PI; 
+       // Smooth oscillation -> MAKES IT MOVE DYNAMICALLY 
+       if(reverse) {
+        angle = -angle;
+    }
+       sketch.translate(sketch.width / 2, sketch.height);
+       sketch.branch(100);
+     }
+
+   sketch.branch = function(len) {
+       sketch.line(0, 0, 0, -len);
+       sketch.translate(0, -len);
+       if (len > 4) {
+           sketch.push();
+           sketch.rotate(angle); // Rotate right
+           sketch.branch(len * 0.67);
+           sketch.pop();
+           sketch.push();
+           sketch.rotate(-angle); // Rotate left
+           sketch.branch(len * 0.67);
+           sketch.pop();
+       }
+   }
 }
 
-function branch(len) {
-  line(0, 0, 0, -len);
-  translate(0, -len);
-  if (len > 4) {
-    push();
-    rotate(angle); // Rotate right
-    branch(len * 0.67);
-    pop();
-    push();
-    rotate(-angle); // Rotate left
-    branch(len * 0.67);
-    pop();
-  }
-}
+new p5(tree2);
